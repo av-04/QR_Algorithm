3fe3b6b6c29d5b7d278b4715bbf393d81e489944
l <84463969+av-04@users.noreply.github.com>
2026-05-13 15:07:51 +0530

    Update reed_solomon.py

--- reed_solomon.py
+++ reed_solomon.py
@@ -16,3 +16,14 @@ def gen_poly(nsym, log, exp):
         g = polymul(g, [1, exp[i]], log, exp)
 
     return g
+
+
+def reedencode(data, nsym, log, exp):
+    gen = gen_poly(nsym, log, exp)
+    msg = data + [0] * nsym
+    for i in range(len(data)):
+        coef = msg[i]
+        if coef != 0:
+            for j in range(len(gen)):
+                msg[i + j] ^= gf256.mul(coef, gen[j], log, exp)
+    return msg[-nsym:]
